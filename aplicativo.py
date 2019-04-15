a2649fd7cdbf3c6792af43d1a8fc9b486450b8c3
jjoaovitor7 <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-15 14:15:34 -0300

    background modificado p/ preto e mais.
    adicionada lista sitesDW e variável siteDW p/ armazenar o site escolhido aleatoriamente.
    adicionado um site na lista sitesDW.

--- aplicativo.py
+++ aplicativo.py
@@ -4,5 +4,9 @@ import random
 janela = Tk()
 janela.title("Deep Web Link Aleatório")
 janela.geometry("300x150")
+janela.configure(bg="black")
+
+sitesDW = ["Facebook - https://www.facebookcorewwwi.onion/", ]
+siteDW = random.choice(sitesDW)
 
 janela.mainloop()
