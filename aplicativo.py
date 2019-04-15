8cf31b8aea8d2943a0f8bd25bb61d36350e47e43
jjoaovitor7 <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-15 14:25:36 -0300

    adicionado título e mais.
    adicionado título. 
    título p/ cor verde c/ fundo preto.

--- aplicativo.py
+++ aplicativo.py
@@ -6,6 +6,10 @@ janela.title("Deep Web Link Aleatório")
 janela.geometry("300x150")
 janela.configure(bg="black")
 
+titulo =  Label(janela, text="Deep Web Link Aleatório", font=("Comic Sans MS", "10", "bold", "italic"))
+titulo.grid(column=0, row=0)
+titulo.configure(bg="black", fg ="#00ff00")
+
 sitesDW = ["Facebook - https://www.facebookcorewwwi.onion/", ]
 siteDW = random.choice(sitesDW)
 
