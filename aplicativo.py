d456393fbdb2e9cfe44facf1328c778d1a3753a2
jjoaovitor7 <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-15 14:54:50 -0300

    várias modificações e adições.
    dimensões da janela modificadas. modificação na posição do titulo. adicionado label URL.
    adicionado botão e evento quando o botão é clicado. 
    adicionado mais um site na lista sitesDW.

--- aplicativo.py
+++ aplicativo.py
@@ -3,14 +3,24 @@ import random
 
 janela = Tk()
 janela.title("Deep Web Link Aleatório")
-janela.geometry("300x150")
+janela.geometry("350x100")
 janela.configure(bg="black")
 
-titulo =  Label(janela, text="Deep Web Link Aleatório", font=("Comic Sans MS", "10", "bold", "italic"))
-titulo.grid(column=0, row=0)
-titulo.configure(bg="black", fg ="#00ff00")
+titulo = Label(janela, text="Deep Web Link Aleatório", font=("Comic Sans MS", "10", "bold", "italic"))
+titulo.grid(column=0, row=0, sticky=W)
+titulo.configure(bg="black", fg="#00ff00")
 
-sitesDW = ["Facebook - https://www.facebookcorewwwi.onion/", ]
-siteDW = random.choice(sitesDW)
+url = Label(janela, text="URL", font=("Comic Sans MS", "9"))
+url.grid(column=0, row=2)
+url.configure(bg="black", fg ="#00ff00")
+
+def clicado():
+        sitesDW = ["Facebook - https://www.facebookcorewwwi.onion", "notEvil - https://hss3uro2hsxfogfq.onion"]
+        siteDW = random.choice(sitesDW)
+        url.configure(text=siteDW)
+        
+siteBotao = Button(janela, text = "Go!", command=clicado)
+siteBotao.grid(column=0, row=4)
+siteBotao.configure(bg="black", fg="#00ff00")
 
 janela.mainloop()
