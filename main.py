b277e24e2cf1b60722100bcdfcc02a4854f00930
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-16 08:33:42 -0300

    adicionado menu com temas e temas.

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ janela.title("Deep Web Link Aleatório")
 janela.geometry("280x100")
 janela.configure(bg="black")
 
+menubar = Menu(janela)
+janela.config(menu=menubar)
+opcaomenu1 = Menu(menubar)
+menubar.add_cascade(label='Temas', menu=opcaomenu1)
+
 titulo = Label(janela, text="Deep Web Link Aleatório", font=("Comic Sans MS", "10", "bold", "italic"))
 titulo.grid(column=0, row=0, sticky=W)
 titulo.configure(bg="black", fg="#00ff00")
@@ -22,5 +27,20 @@ def clicado():
 siteBotao = Button(janela, text = "Go!", command=clicado)
 siteBotao.grid(column=0, row=4)
 siteBotao.configure(bg="black", fg="#00ff00")
+
+def TemaBranco():
+        janela.configure(bg="white")
+        titulo.configure(bg="white", fg="black")
+        url.configure(bg="white", fg="black")
+        siteBotao.configure(bg="white", fg="black")
+
+def TemaPreto():
+        janela.configure(bg="black")
+        titulo.configure(bg="black", fg="#00ff00")
+        url.configure(bg="black", fg="#00ff00")
+        siteBotao.configure(bg="black", fg="#00ff00")
+
+opcaomenu1.add_command(label='Tema Branco', command=TemaBranco)
+opcaomenu1.add_command(label='Tema Preto', command=TemaPreto)
 
 janela.mainloop()
