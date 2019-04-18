96323b6cf6e440ca577ed358a6430478fb41244d
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-18 15:20:32 -0300

    Update ajudaNavegadores.py

--- ajudaNavegadores.py
+++ ajudaNavegadores.py
@@ -4,6 +4,7 @@ def ajudaNavegadores():
 
     janelaNavegadores = Tk()
     janelaNavegadores.title('Navegadores')
+    janelaNavegadores.wm_iconbitmap("dw_ico.ico")
 
     redeOnion = Label(janelaNavegadores, text="Rede onion: https://www.torproject.org/download/", font=("Times New Roman", "11"))
     redeI2P = Label(janelaNavegadores, text="Rede i2p: https://geti2p.net/pt-br/download", font=("Times New Roman", "11"))
