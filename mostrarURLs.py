675f6396a8d5977784d18f74287c3eb0e6d951a5
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-18 14:08:38 -0300

    adicionada mais uma URL.

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -6,7 +6,7 @@ def mostrarURLs():
          janelaURL.title('URLs')
          
          urlsDoPrograma = ["notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "Torch - xmh57jrzrnw6insl.onion",
-                   "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion",
+                   "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion", "The New York Times - nytimes3xbfgragh.onion",
                    "Bible4u - bible4u2kjgjvbxs.onion", "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion",
                    "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "Facebook - facebookcorewwwi.onion"]
 
