8f644689c26d08869b4b7895fff6a758498ea6ac
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-18 07:22:28 -0300

    Create mostrarURLs.py

---
+++ mostrarURLs.py
@@ -0,0 +1,15 @@
+def mostrarURLs():
+         
+         janelaURL = Tk()
+         janelaURL.title('URLs')
+         
+         urlsDoPrograma = ["notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "Torch - xmh57jrzrnw6insl.onion",
+                   "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion",
+                   "Bible4u - bible4u2kjgjvbxs.onion", "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion",
+                   "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "Facebook - facebookcorewwwi.onion"]
+
+         x=0
+         for url in urlsDoPrograma:
+             urlPrograma = Label(janelaURL, text=url, font=fonte1)
+             urlPrograma.grid(column=0, row=x)
+             x+=1
