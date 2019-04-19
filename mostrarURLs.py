e90864e5d697a14eb6161936098d8745a832508e
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-19 20:07:26 -0300

    adicionada mais duas URLs.

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -1,15 +1,14 @@
 from tkinter import *
 
-def mostrarURLs():
-         
+def mostrarURLs():      
          janelaURL = Tk()
          janelaURL.title('URLs')
          janelaURL.wm_iconbitmap("dw_ico.ico")
          
-         urlsDoPrograma = ["notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "Torch - xmh57jrzrnw6insl.onion",
+         urlsDoPrograma = ["Bible4u - bible4u2kjgjvbxs.onion", "notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "Torch - xmh57jrzrnw6insl.onion",
                    "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion", "The New York Times - nytimes3xbfgragh.onion",
-                   "Bible4u - bible4u2kjgjvbxs.onion", "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion",
-                   "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "Facebook - facebookcorewwwi.onion"]
+                   "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion", "Planet Debian - gnvweaoe2xzjqldu.onion",
+                   "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "CryptoShare - qtx6d2ggmma6hgio.onion", "Facebook - facebookcorewwwi.onion"]
 
          x=0
          for url in urlsDoPrograma:
