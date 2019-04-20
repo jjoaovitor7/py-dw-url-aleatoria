f64e0df58826fd67200b78cb4ec4b9745471180a
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-04-20 11:05:42 -0300

    adicionada mais uma URL.

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -5,7 +5,7 @@ def mostrarURLs():
          janelaURL.title('URLs')
          janelaURL.wm_iconbitmap("dw_ico.ico")
          
-         urlsDoPrograma = ["Bible4u - bible4u2kjgjvbxs.onion", "notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "SearX - 5plvrsgydwy2sgce.onion",
+         urlsDoPrograma = ["Bible4u - bible4u2kjgjvbxs.onion", "notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "SearX - 5plvrsgydwy2sgce.onion", "SearX - juy4e6eicawzdrz7.onion", 
                    "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion", "The New York Times - nytimes3xbfgragh.onion",
                    "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion", "Planet Debian - gnvweaoe2xzjqldu.onion",
                    "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "CryptoShare - qtx6d2ggmma6hgio.onion", "Facebook - facebookcorewwwi.onion"]
