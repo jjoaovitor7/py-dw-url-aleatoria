a1fe12976b1aef8d94bd94c6a71dcccc0552e143
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-03 14:16:26 -0300

    mudança de cor.
    mudança de cor (bg da janela e do bg e fg do label).

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -5,6 +5,7 @@ def mostrar_urls():
          janelaURL=Toplevel()
          janelaURL.title('URLs')
          janelaURL.wm_iconbitmap('dw_ico.ico')
+         janelaURL.configure(bg='black')
          
          conn=sqlite3.connect('urls.db')
          cursor=conn.cursor()
@@ -14,7 +15,8 @@ def mostrar_urls():
          
          x=0
          for url in cursor.fetchall():
-             urlPrograma=Label(janelaURL, text=url, font=("Times New Roman", "11"))
+             urlPrograma=Label(janelaURL, text=url, font=('Times New Roman', '11'))
+             urlPrograma.configure(bg='black', fg='#00ff00')
              urlPrograma.grid(column=0, row=x)
              x+=1          
          
