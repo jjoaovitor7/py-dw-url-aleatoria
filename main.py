80d1caacb8588477dc0c390a4c121fc1b13ed12b
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-03 13:49:49 -0300

    organização do código.
    aspas duplas p/ aspas simples, com exceção das urls em sitesDW.
    opcaomenu1 p/ temas.
    opcaomenu2 p/ ajuda.
    sticky do titulo.grid de W p/ N.
    largura da janela p/ 300.

--- main.py
+++ main.py
@@ -1,63 +1,62 @@
 from tkinter import *
-import random
-import webbrowser
+import random, webbrowser
 from mostrarURLs import *
 from ajudaNavegadores import *
 
-janela = Tk()
-janela.title("Deep Web Link Aleatório")
-janela.wm_iconbitmap("dw_ico.ico")
-janela.geometry("280x100")
-janela.configure(bg="black")
+janela=Tk()
+janela.title('Deep Web Link Aleatório')
+janela.wm_iconbitmap('dw_ico.ico')
+janela.geometry('300x100')
+janela.configure(bg='black')
 
-fonte1 = ("Comic Sans MS", "10")
+fonte1=('Comic Sans MS', '10')
 
-menubar = Menu(janela)
+menubar=Menu(janela)
 janela.config(menu=menubar)
-opcaomenu1 = Menu(menubar)
-menubar.add_cascade(label='Temas', menu=opcaomenu1)
+temas=Menu(menubar)
+menubar.add_cascade(label='Temas', menu=temas)
     
-opcaomenu2 = Menu(menubar)
-menubar.add_cascade(label='Ajuda', menu=opcaomenu2)
-opcaomenu2.add_command(label='URLs', command=mostrarURLs)
-opcaomenu2.add_command(label='Navegadores', command=ajudaNavegadores)
+ajuda = Menu(menubar)
+menubar.add_cascade(label='Ajuda', menu=ajuda)
+ajuda.add_command(label='URLs', command=mostrar_urls)
+ajuda.add_command(label='Navegadores', command=ajudaNavegadores)
 
-titulo = Label(janela, text="Deep Web Link Aleatório", font=("Comic Sans MS", "10", "bold"))
-titulo.grid(column=0, row=0, sticky=W)
-titulo.configure(bg="black", fg="#00ff00")
+titulo=Label(janela, text='Deep Web Link Aleatório', font=('Comic Sans MS', '10', 'bold'))
+titulo.grid(column=0, row=0, sticky=N)
+titulo.configure(bg='black', fg='#00ff00')
 
-url = Label(janela, text="URL", font=fonte1)
+url=Label(janela, text="URL", font=fonte1)
 url.grid(column=0, row=2)
-url.configure(bg="black", fg ="#00ff00")
+url.configure(bg='black', fg ='#00ff00')
 
 def botaoGoClicado():        
-        sitesDW = ["facebookcorewwwi.onion", "hss3uro2hsxfogfq.onion", "theches3nacocgsc.onion", "qtx6d2ggmma6hgio.onion" 
+        sitesDW=["facebookcorewwwi.onion", "hss3uro2hsxfogfq.onion", "theches3nacocgsc.onion", "qtx6d2ggmma6hgio.onion" 
                    "archivecrfip2lpi.onion", "3g2upl4pq6kufc4m.onion", "5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion", 
                    "bible4u2kjgjvbxs.onion", "enotegggr635n4lw.onion", "nytimes3xbfgragh.onion", "juy4e6eicawzdrz7.onion", "nzxj65x32vh2fkhk.onion",
                    "duskgytldkxiuqc6.onion", "z2huz7tsxluvnxoc.onion", "gnvweaoe2xzjqldu.onion" "5plvrsgydwy2sgce.onion", "searchb5a7tmimez.onion", "nxhhwbbxc4khvvlw.onion", "nrybuqtxgxnavtla.onion", "eljwdzi4pgrrlwwq.onion"]
-        siteDW = random.choice(sitesDW)
+        siteDW=random.choice(sitesDW)
         def clicarLink(event):
                 webbrowser.open_new(siteDW)
         url.configure(text=siteDW)
         url.bind("<Button-1>", clicarLink)
         
-siteBotao = Button(janela, text="Go!", command=botaoGoClicado)
+siteBotao=Button(janela, text='Go!', command=botaoGoClicado)
 siteBotao.grid(column=0, row=4)
-siteBotao.configure(bg="black", fg="#00ff00")
+siteBotao.configure(bg='black', fg='#00ff00')
 
 def TemaBranco():        
-        janela.configure(bg="white")
-        titulo.configure(bg="white", fg="black")
-        url.configure(bg="white", fg="black")
-        siteBotao.configure(bg="white", fg="black")
+        janela.configure(bg='white')
+        titulo.configure(bg='white', fg='black')
+        url.configure(bg='white', fg='black')
+        siteBotao.configure(bg='white', fg='black')
 
 def TemaPreto():      
-        janela.configure(bg="black")
-        titulo.configure(bg="black", fg="#00ff00")
-        url.configure(bg="black", fg="#00ff00")
-        siteBotao.configure(bg="black", fg="#00ff00")
+        janela.configure(bg='black')
+        titulo.configure(bg='black', fg='#00ff00')
+        url.configure(bg='black', fg='#00ff00')
+        siteBotao.configure(bg='black', fg='#00ff00')
 
-opcaomenu1.add_command(label='Tema Branco', command=TemaBranco)
-opcaomenu1.add_command(label='Tema Preto', command=TemaPreto)
+temas.add_command(label='Tema Branco', command=TemaBranco)
+temas.add_command(label='Tema Preto', command=TemaPreto)
 
 janela.mainloop()
