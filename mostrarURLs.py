fd981be36ade70cea5bc55481f2e87d6b84e1186
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-03 13:33:16 -0300

    adicionada conexão c/ banco de dados.
    janelaURL = Tk() p/ janelaURL = Toplevel().
    adicionada conexão c/ banco de dados.
    mostrarURLs p/ mostrar_urls.

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -1,17 +1,21 @@
 from tkinter import *
+import sqlite3
 
-def mostrarURLs():      
-         janelaURL = Tk()
+def mostrar_urls():      
+         janelaURL=Toplevel()
          janelaURL.title('URLs')
-         janelaURL.wm_iconbitmap("dw_ico.ico")
+         janelaURL.wm_iconbitmap('dw_ico.ico')
          
-         urlsDoPrograma = ["Bible4u - bible4u2kjgjvbxs.onion", "notEvil - hss3uro2hsxfogfq.onion", "The Chess - theches3nacocgsc.onion", "SearX - 5plvrsgydwy2sgce.onion", "SearX - juy4e6eicawzdrz7.onion", "SearX - searchb5a7tmimez.onion", "SearX - nxhhwbbxc4khvvlw.onion", "SearX - nrybuqtxgxnavtla.onion", "SearX - eljwdzi4pgrrlwwq.onion", 
-                   "Internet Archive - archivecrfip2lpi.onion", "DuckDuckGo - 3g2upl4pq6kufc4m.onion", "Web Shell Archive - z2huz7tsxluvnxoc.onion", "The New York Times - nytimes3xbfgragh.onion", "Stronghold Paste - nzxj65x32vh2fkhk.onion",
-                   "Enot - enotegggr635n4lw.onion", "TorPaste - 5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion", "Planet Debian - gnvweaoe2xzjqldu.onion",
-                   "Example Rendezvous Points Page - duskgytldkxiuqc6.onion", "CryptoShare - qtx6d2ggmma6hgio.onion", "Facebook - facebookcorewwwi.onion"]
-
+         conn=sqlite3.connect('urls.db')
+         cursor=conn.cursor()
+         cursor.execute("""
+         SELECT nome, url FROM urls ;
+         """)
+         
          x=0
-         for url in urlsDoPrograma:
-             urlPrograma = Label(janelaURL, text=url, font=("Times New Roman", "11"))
+         for url in cursor.fetchall():
+             urlPrograma=Label(janelaURL, text=url, font=("Times New Roman", "11"))
              urlPrograma.grid(column=0, row=x)
-             x+=1
+             x+=1          
+         
+         conn.close()
