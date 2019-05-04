28c796992c1790c19f4a869dc64f97bc9b5972a9
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-04 17:02:54 -0300

    várias mudanças.
    fonte1 removida.
    botaoGoClicado() p/ botao_go_clicado()
    sitesDW e siteDW removidos.
    conexão c/ o banco de dados adicionada.
    clicarLink() p/ clicar_url()
    erro no webbrowser.open_new(url_)

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ janela.wm_iconbitmap('dw_ico.ico')
 janela.geometry('300x100')
 janela.configure(bg='black')
 
-fonte1=('Comic Sans MS', '10')
-
 menubar=Menu(janela)
 janela.config(menu=menubar)
+
 temas=Menu(menubar)
 menubar.add_cascade(label='Temas', menu=temas)
-    
-ajuda = Menu(menubar)
+
+ajuda=Menu(menubar)
 menubar.add_cascade(label='Ajuda', menu=ajuda)
 ajuda.add_command(label='URLs', command=mostrar_urls)
 ajuda.add_command(label='Navegadores', command=ajuda_navegadores)
@@ -25,22 +24,24 @@ titulo=Label(janela, text='Deep Web Link Aleatório', font=('Comic Sans MS', '10
 titulo.grid(column=0, row=0, sticky=N)
 titulo.configure(bg='black', fg='#00ff00')
 
-url=Label(janela, text="URL", font=fonte1)
+url=Label(janela, text="URL", font=('Comic Sans MS', '10'))
 url.grid(column=0, row=2)
 url.configure(bg='black', fg ='#00ff00')
 
-def botaoGoClicado():        
-        sitesDW=["facebookcorewwwi.onion", "hss3uro2hsxfogfq.onion", "theches3nacocgsc.onion", "qtx6d2ggmma6hgio.onion" 
-                   "archivecrfip2lpi.onion", "3g2upl4pq6kufc4m.onion", "5y5ek6tlzttcxgvlknf5mxybbsntfqsq2q2fngdbfmskgghukfkn5uqd.onion", 
-                   "bible4u2kjgjvbxs.onion", "enotegggr635n4lw.onion", "nytimes3xbfgragh.onion", "juy4e6eicawzdrz7.onion", "nzxj65x32vh2fkhk.onion",
-                   "duskgytldkxiuqc6.onion", "z2huz7tsxluvnxoc.onion", "gnvweaoe2xzjqldu.onion" "5plvrsgydwy2sgce.onion", "searchb5a7tmimez.onion", "nxhhwbbxc4khvvlw.onion", "nrybuqtxgxnavtla.onion", "eljwdzi4pgrrlwwq.onion"]
-        siteDW=random.choice(sitesDW)
-        def clicarLink(event):
-                webbrowser.open_new(siteDW)
-        url.configure(text=siteDW)
-        url.bind("<Button-1>", clicarLink)
+def botao_go_clicado():        
+        conn=sqlite3.connect('urls.db')
+        cursor=conn.cursor()
+        cursor.execute("""
+        SELECT url FROM urls  ORDER BY RANDOM() LIMIT 1;
+        """)
+        url_=cursor.fetchall()
+        url.configure(text=url_)
+        conn.close()
+        #def clicar_url(event):
+                #webbrowser.open_new(url_) #erro
+        #url.bind('<Button-1>', clicar_url)
         
-siteBotao=Button(janela, text='Go!', command=botaoGoClicado)
+siteBotao=Button(janela, text='Go!', command=botao_go_clicado)
 siteBotao.grid(column=0, row=4)
 siteBotao.configure(bg='black', fg='#00ff00')
 
