a878f6ed1ebd0df0f8eb455515d1d77874a8f2f7
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-05 07:58:29 -0300

    adicionado sticky=W em urlPrograma.grid()

--- mostrarURLs.py
+++ mostrarURLs.py
@@ -6,18 +6,18 @@ def mostrar_urls():
          janelaURL.title('URLs')
          janelaURL.wm_iconbitmap('dw_ico.ico')
          janelaURL.configure(bg='black')
-         
+
          conn=sqlite3.connect('urls.db')
          cursor=conn.cursor()
          cursor.execute("""
          SELECT nome, url FROM urls ;
          """)
-         
+
          x=0
          for url in cursor.fetchall():
              urlPrograma=Label(janelaURL, text=url, font=('Times New Roman', '11'))
              urlPrograma.configure(bg='black', fg='#00ff00')
-             urlPrograma.grid(column=0, row=x)
-             x+=1          
-         
+             urlPrograma.grid(column=0, row=x, sticky=W)
+             x+=1
+
          conn.close()
