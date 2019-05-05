3afd9c70ad56c91d3e1544a95db7fb8ef4990c5f
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-05 12:41:06 -0300

    várias modificações e organização do código.
    titulo p/ lTitulo.
    url p/ lURL.
    siteBotao p/ botao_go.

--- main.py
+++ main.py
@@ -14,18 +14,33 @@ janela.config(menu=menubar)
 temas=Menu(menubar)
 menubar.add_cascade(label='Temas', menu=temas)
 
+def tema_branco():        
+        janela.configure(bg='white')
+        lTitulo.configure(bg='white', fg='black')
+        lURL.configure(bg='white', fg='black')
+        botao_go.configure(bg='white', fg='black')
+
+def tema_preto():      
+        janela.configure(bg='black')
+        lTitulo.configure(bg='black', fg='#00ff00')
+        lURL.configure(bg='black', fg='#00ff00')
+        botao_go.configure(bg='black', fg='#00ff00')
+
+temas.add_command(label='Tema Branco', command=tema_branco)
+temas.add_command(label='Tema Preto', command=tema_preto)
+
 ajuda=Menu(menubar)
 menubar.add_cascade(label='Ajuda', menu=ajuda)
 ajuda.add_command(label='URLs', command=mostrar_urls)
 ajuda.add_command(label='Navegadores', command=ajuda_navegadores)
 
-titulo=Label(janela, text='Deep Web Link Aleatório', font=('Comic Sans MS', '10', 'bold'))
-titulo.grid(column=0, row=0, sticky=N)
-titulo.configure(bg='black', fg='#00ff00')
+lTitulo=Label(janela, text='Deep Web Link Aleatório', font=('Comic Sans MS', '10', 'bold'))
+lTitulo.grid(column=0, row=0, sticky=N)
+lTitulo.configure(bg='black', fg='#00ff00')
 
-url=Label(janela, text="URL", font=('Comic Sans MS', '10'))
-url.grid(column=0, row=2)
-url.configure(bg='black', fg ='#00ff00')
+lURL=Label(janela, text="URL", font=('Comic Sans MS', '10'))
+lURL.grid(column=0, row=2)
+lURL.configure(bg='black', fg ='#00ff00')
 
 def botao_go_clicado():        
         conn=sqlite3.connect('urls.db')
@@ -34,29 +49,14 @@ def botao_go_clicado():
         SELECT url FROM urls  ORDER BY RANDOM() LIMIT 1;
         """)
         url_=cursor.fetchall()
-        url.configure(text=url_)
+        lURL.configure(text=url_)
         conn.close()
         #def clicar_url(event):
                 #webbrowser.open_new(url_) #erro
         #url.bind('<Button-1>', clicar_url)
         
-siteBotao=Button(janela, text='Go!', command=botao_go_clicado)
-siteBotao.grid(column=0, row=4)
-siteBotao.configure(bg='black', fg='#00ff00')
-
-def tema_branco():        
-        janela.configure(bg='white')
-        titulo.configure(bg='white', fg='black')
-        url.configure(bg='white', fg='black')
-        siteBotao.configure(bg='white', fg='black')
-
-def tema_preto():      
-        janela.configure(bg='black')
-        titulo.configure(bg='black', fg='#00ff00')
-        url.configure(bg='black', fg='#00ff00')
-        siteBotao.configure(bg='black', fg='#00ff00')
-
-temas.add_command(label='Tema Branco', command=tema_branco)
-temas.add_command(label='Tema Preto', command=tema_preto)
+botao_go=Button(janela, text='Go!', command=botao_go_clicado)
+botao_go.grid(column=0, row=4)
+botao_go.configure(bg='black', fg='#00ff00')
 
 janela.mainloop()
