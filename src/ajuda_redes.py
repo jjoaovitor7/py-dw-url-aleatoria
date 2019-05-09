836d8f06863307b05592910c2c79bcfc7d3a0c83
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-09 09:59:49 -0300

    organização e lFreenet.configure() adicionado.

--- src/ajuda_redes.py
+++ src/ajuda_redes.py
@@ -12,29 +12,26 @@ def ajuda_redes():
     lOnion=Label(jRedes, 
                  text='Onion: https://www.torproject.org/download/', 
                  font=fonte)
-    
     lI2p=Label(jRedes, 
                text='i2p: https://geti2p.net/pt-br/download', 
                font=fonte)
-    
     lFreenet=Label(jRedes, 
                    text='Freenet: https://freenetproject.org/', 
                    font=fonte)
     
     lOnion.configure(bg='black', 
-                     fg='#00ff00')
-    
+                     fg='#00ff00')  
     lI2p.configure(bg='black', 
                    fg='#00ff00')
+    lFreenet.configure(bg='black', 
+                       fg='#00ff00')
     
     lOnion.grid(column=0, 
                 row=0, 
                 sticky=W)
-    
     lI2p.grid(column=0, 
               row=1, 
               sticky=W)
-    
     lFreenet.grid(column=0, 
                   row=2, 
                   sticky=W)
