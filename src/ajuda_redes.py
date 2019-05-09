8a25d52384d3b0021ba10e4b0396781ac0322478
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-09 10:10:38 -0300

    organização.

--- src/ajuda_redes.py
+++ src/ajuda_redes.py
@@ -9,29 +9,14 @@ def ajuda_redes():
     
     jRedes.configure(bg='black')
     
-    lOnion=Label(jRedes, 
-                 text='Onion: https://www.torproject.org/download/', 
-                 font=fonte)
-    lI2p=Label(jRedes, 
-               text='i2p: https://geti2p.net/pt-br/download', 
-               font=fonte)
-    lFreenet=Label(jRedes, 
-                   text='Freenet: https://freenetproject.org/', 
-                   font=fonte)
+    lOnion=Label(jRedes, text='Onion: https://www.torproject.org/download/', font=fonte)
+    lI2p=Label(jRedes, text='i2p: https://geti2p.net/pt-br/download', font=fonte)
+    lFreenet=Label(jRedes, text='Freenet: https://freenetproject.org/', font=fonte)
     
-    lOnion.configure(bg='black', 
-                     fg='#00ff00')  
-    lI2p.configure(bg='black', 
-                   fg='#00ff00')
-    lFreenet.configure(bg='black', 
-                       fg='#00ff00')
+    lOnion.configure(bg='black', fg='#00ff00')  
+    lI2p.configure(bg='black', fg='#00ff00')
+    lFreenet.configure(bg='black', fg='#00ff00')
     
-    lOnion.grid(column=0, 
-                row=0, 
-                sticky=W)
-    lI2p.grid(column=0, 
-              row=1, 
-              sticky=W)
-    lFreenet.grid(column=0, 
-                  row=2, 
-                  sticky=W)
+    lOnion.grid(column=0, row=0, sticky=W)
+    lI2p.grid(column=0, row=1, sticky=W)
+    lFreenet.grid(column=0, row=2, sticky=W)
