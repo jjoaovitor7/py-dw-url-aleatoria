1cb03f4ce569b2b0299839e9aae97c9afb1430f0
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-16 15:40:13 -0300

    organização do código.

--- src/mostrar_urls.py
+++ src/mostrar_urls.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
-import sqlite3, platform
+import platform, sqlite3
 from ajuda_redes import *
 
 def mostrar_urls():
-         so=platform.system()
-         jURL=Toplevel()
-         jURL.title('URLs')
-         if so=='Windows':
-                 jURL.wm_iconbitmap('ico\dw_ico.ico')
-         if so=='Linux':
-                 jURL.wm_iconbitmap('@ico/dw_ico.xbm')
-         jURL.configure(bg='black')
-         
-         menubar=Menu(jURL)
-         jURL.config(menu=menubar)
-         
-         ajuda=Menu(menubar, tearoff=0)
-         menubar.add_cascade(label='Ajuda', menu=ajuda)
-         ajuda.add_command(label='Redes', command=ajuda_redes)
+    so=platform.system()
+    jURL=Toplevel()
+    jURL.title('URLs')
+    if so=='Windows':
+            jURL.wm_iconbitmap('ico\dw_ico.ico')
+    if so=='Linux':
+            jURL.wm_iconbitmap('@ico/dw_ico.xbm')
+    jURL.configure(bg='black')
+     
+    menubar=Menu(jURL)
+    jURL.config(menu=menubar)
+     
+    ajuda=Menu(menubar, tearoff=0)
+    menubar.add_cascade(label='Ajuda', menu=ajuda)
+    ajuda.add_command(label='Redes', command=ajuda_redes)
 
-         conn=sqlite3.connect('urls.db')
-         cursor=conn.cursor()
-         cursor.execute("""
-         SELECT nome, url FROM urls;
-         """)
-         x=0
-         for url in cursor.fetchall():
-             lURL=Label(jURL, text=url, font=('Times New Roman', '11'))
-             lURL.configure(bg='black', fg='#00ff00')
-             lURL.grid(column=0, row=x, sticky=W)
-             x+=1
-         conn.close()
+    conn=sqlite3.connect('urls.db')
+    cursor=conn.cursor()
+    cursor.execute("""
+    SELECT nome, url FROM urls;
+    """)
+    x=0
+    for url in cursor.fetchall():
+        lURL=Label(jURL, text=url, font=('Times New Roman', '11'))
+        lURL.configure(bg='black', fg='#00ff00')
+        lURL.grid(column=0, row=x, sticky=W)
+        x+=1
+    conn.close()
