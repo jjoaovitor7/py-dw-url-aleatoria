374604c8f4ae21bec8078a43f0503e01af882e3e
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-17 11:52:40 -0300

    adequando (ou tentando adequar) o código ao PEP 8.

--- src/mostrar_urls.py
+++ src/mostrar_urls.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
-import platform, sqlite3
-from ajuda_redes import *
+import platform
+import sqlite3
+from ajuda_redes import ajuda_redes
 
 def mostrar_urls():
-    so=platform.system()
-    jURL=Toplevel()
+    jURL = Toplevel()
     jURL.title('URLs')
-    if so=='Windows':
-            jURL.wm_iconbitmap('ico\dw_ico.ico')
-    if so=='Linux':
-            jURL.wm_iconbitmap('@ico/dw_ico.xbm')
-    jURL.configure(bg='black')
-     
-    menubar=Menu(jURL)
-    jURL.config(menu=menubar)
-     
-    ajuda=Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Ajuda', menu=ajuda)
-    ajuda.add_command(label='Redes', command=ajuda_redes)
+    jURL.configure(bg = 'black')
 
-    conn=sqlite3.connect('urls.db')
-    cursor=conn.cursor()
+    so = platform.system()
+    if so == 'Linux':
+       jURL.wm_iconbitmap('@ico/dw_ico.xbm')
+    if so == 'Windows':
+       jURL.wm_iconbitmap('ico\dw_ico.ico')
+    
+    menubar = Menu(jURL)
+    jURL.config(menu = menubar)
+     
+    ajuda = Menu(menubar, tearoff = 0)
+    menubar.add_cascade(label = 'Ajuda', menu = ajuda)
+    ajuda.add_command(label = 'Redes', command = ajuda_redes)
+
+    conn = sqlite3.connect('urls.db')
+    cursor = conn.cursor()
     cursor.execute("""
     SELECT nome, url FROM urls;
     """)
-    x=0
+    
+    x = 0
+    fonte = ('Times New Roman', '11')
     for url in cursor.fetchall():
-        lURL=Label(jURL, text=url, font=('Times New Roman', '11'))
-        lURL.configure(bg='black', fg='#00ff00')
-        lURL.grid(column=0, row=x, sticky=W)
-        x+=1
+        lURL = Label(jURL, text = url, font = fonte)
+        lURL.configure(bg = 'black', fg = '#00ff00')
+        lURL.grid(column = 0, row = x, sticky = W)
+        x += 1
+    
     conn.close()
