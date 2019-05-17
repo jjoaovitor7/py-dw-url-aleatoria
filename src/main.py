0ee1a70a0b82c734a1f185dc4dfd015e32d4d778
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-17 11:44:53 -0300

    adequando (ou tentando adequar) o código ao PEP 8.

--- src/main.py
+++ src/main.py
@@ -1,70 +1,74 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
-import platform, sqlite3, webbrowser
-from mostrar_urls import *
-from ajuda_redes import *
+import platform
+import sqlite3
+import webbrowser
+from mostrar_urls import mostrar_urls
+from ajuda_redes import ajuda_redes
 
 def main():
-    so=platform.system()
-    jMain=Tk()
+    jMain = Tk()
     jMain.title('Deep Web URL Aleatório')
-    if so=='Windows':
-            jMain.wm_iconbitmap('ico\dw_ico.ico')
-    if so=='Linux':
+    jMain.configure(bg = 'black')
+    
+    so = platform.system()
+    if so == 'Linux':
             jMain.wm_iconbitmap('@ico/dw_ico.xbm')
-    jMain.configure(bg='black')
+    if so == 'Windows':
+            jMain.wm_iconbitmap('ico\dw_ico.ico')
 
-    menubar=Menu(jMain)
-    jMain.config(menu=menubar)
+    menubar = Menu(jMain)
+    jMain.config(menu = menubar)
 
-    temas=Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Temas', menu=temas)
+    temas = Menu(menubar, tearoff = 0)
+    menubar.add_cascade(label = 'Temas', menu = temas)
 
+    ajuda = Menu(menubar, tearoff = 0)
+    menubar.add_cascade(label = 'Ajuda', menu = ajuda)
+    ajuda.add_command(label = 'URLs', command = mostrar_urls)
+    ajuda.add_command(label = 'Redes', command = ajuda_redes)
+
+    lTitulo = Label(jMain, text = 'Deep Web URL Aleatório', font = ('Comic Sans MS', '10', 'bold'))
+    lTitulo.grid(column = 0, row = 0, sticky = N)
+    lTitulo.configure(bg = 'black', fg = '#00ff00')
+    
     def tema_branco():        
-        jMain.configure(bg='white')
-        lTitulo.configure(bg='white', fg='black')
-        lURL.configure(bg='white', fg='black')
-        botao_go.configure(bg='white', fg='black')
+        jMain.configure(bg = 'white')
+        lTitulo.configure(bg = 'white', fg = 'black')
+        lURL.configure(bg = 'white', fg = 'black')
+        botao_go.configure(bg = 'white', fg = 'black')
 
     def tema_preto():      
-        jMain.configure(bg='black')
-        lTitulo.configure(bg='black', fg='#00ff00')
-        lURL.configure(bg='black', fg='#00ff00')
-        botao_go.configure(bg='black', fg='#00ff00')
+        jMain.configure(bg = 'black')
+        lTitulo.configure(bg = 'black', fg = '#00ff00')
+        lURL.configure(bg = 'black', fg = '#00ff00')
+        botao_go.configure(bg = 'black', fg = '#00ff00')
 
-    temas.add_command(label='Tema Branco', command=tema_branco)
-    temas.add_command(label='Tema Preto', command=tema_preto)
-
-    ajuda=Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Ajuda', menu=ajuda)
-    ajuda.add_command(label='URLs', command=mostrar_urls)
-    ajuda.add_command(label='Redes', command=ajuda_redes)
-
-    lTitulo=Label(jMain, text='Deep Web URL Aleatório', font=('Comic Sans MS', '10', 'bold'))
-    lTitulo.grid(column=0, row=0, sticky=N)
-    lTitulo.configure(bg='black', fg='#00ff00')
-
-    lURL=Label(jMain, text='URL', font=('Comic Sans MS', '10'))
-    lURL.grid(column=0, row=2)
-    lURL.configure(bg='black', fg ='#00ff00')
+    temas.add_command(label = 'Tema Branco', command = tema_branco)
+    temas.add_command(label = 'Tema Preto', command = tema_preto)
+    
+    fonte = ('Comic Sans MS', '10'))
+    lURL = Label(jMain, text = 'URL', font = fonte)
+    lURL.grid(column = 0, row = 2)
+    lURL.configure(bg = 'black', fg = '#00ff00')
 
     def botao_go_clicado():        
-        conn=sqlite3.connect('urls.db')
-        cursor=conn.cursor()
+        conn = sqlite3.connect('urls.db')
+        cursor = conn.cursor()
         cursor.execute("""
         SELECT url FROM urls  ORDER BY RANDOM() LIMIT 1;
         """)
-        url_=cursor.fetchall()
-        url2_=str(url_).strip('()[],\'\'')
-        lURL.configure(text=url_)
+        url_ = cursor.fetchall()
+        url2_ = str(url_).strip('()[],\'\'')
+        lURL.configure(text = url_)
         conn.close()
         def clicar_url(event):
             webbrowser.open_new(url2_)
         lURL.bind('<Button-1>', clicar_url)
             
-    botao_go=Button(jMain, text='Go!', command=botao_go_clicado)
-    botao_go.grid(column=0, row=4)
-    botao_go.configure(bg='black', fg='#00ff00')
+    botao_go = Button(jMain, text = 'Go!', command = botao_go_clicado)
+    botao_go.grid(column = 0, row = 4)
+    botao_go.configure(bg = 'black', fg = '#00ff00')
 
     jMain.mainloop()
         
