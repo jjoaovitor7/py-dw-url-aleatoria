3740e378c4bf2026c4730a953137fb98cba5931b
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-07-12 08:59:31 -0300

    #!/usr/bin/env python3 e adequando o código ao PEP8.
    #!/usr/bin/env python3
    
    E251 unexpected spaces around keyword / parameter equals.
    29:23, 29:25, 31:51, 31:53, 32:35, 32:37, 33:29, 33:31, 41:21, 41:23, 43:37, 43:39, 44:30, 44:32, 44:46, 44:48, 45:28, 45:30, 45:46, 45:48, 48:29, 48:31, 49:30, 49:32, 51:27, 51:29, 52:28, 52:30, 54:31, 54:33, 57:21, 57:23, 57:33, 57:35, 57:45, 57:47, 58:19, 58:21, 58:31, 58:33, 58:43, 58:45, 59:23, 59:25, 59:35, 59:37, 59:47, 59:49.
    
    E302 expected 2 blank lines, found 1.
    11:1, 61:1.

--- src/ajuda_redes.py
+++ src/ajuda_redes.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import ttk
@@ -6,6 +7,7 @@ import sqlite3
 
 import definir_cor
 import mostrar_urls
+
 
 def ajuda_redes(self):
     def configurar_janela():
@@ -25,11 +27,11 @@ def ajuda_redes(self):
             print('dw_ico.ico não foi encontrado na pasta ico.')
 
     def tema_preto():
-        self.config(bg = definir_cor.COR_PRETA)
+        self.config(bg=definir_cor.COR_PRETA)
         style = ttk.Style()
-        style.configure('redes.TLabel', background = definir_cor.COR_PRETA,
-                        foreground = definir_cor.COR_VERDE_CLARO,
-                        font = ('Times New Roman', '14'))
+        style.configure('redes.TLabel', background=definir_cor.COR_PRETA,
+                        foreground=definir_cor.COR_VERDE_CLARO,
+                        font=('Times New Roman', '14'))
 
     configurar_janela()
     configurar_icone()
@@ -37,25 +39,26 @@ def ajuda_redes(self):
     tema_preto()
 
     menubar = tk.Menu(self)
-    self.config(menu = menubar)
+    self.config(menu=menubar)
 
-    ajuda = tk.Menu(menubar, tearoff = 0)
-    menubar.add_cascade(label = 'Ajuda', menu = ajuda)
-    ajuda.add_command(label = 'URLs', command = mostrar_urls.run)
+    ajuda = tk.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label='Ajuda', menu=ajuda)
+    ajuda.add_command(label='URLs', command=mostrar_urls.run)
 
     l_onion = ttk.Label(self,
-                        text = 'Onion: https://www.torproject.org/download',
-                        style = 'redes.TLabel')
+                        text='Onion: https://www.torproject.org/download',
+                        style='redes.TLabel')
     l_i2p = ttk.Label(self,
-                      text = 'i2p: https://geti2p.net/pt-br/download',
-                      style = 'redes.TLabel')
+                      text='i2p: https://geti2p.net/pt-br/download',
+                      style='redes.TLabel')
     l_freenet = ttk.Label(self,
-                          text = 'Freenet: https://freenetproject.org',
-                          style = 'redes.TLabel')
+                          text='Freenet: https://freenetproject.org',
+                          style='redes.TLabel')
 
-    l_onion.grid(row = 0, column = 0, sticky = tk.W)
-    l_i2p.grid(row = 1, column = 0, sticky = tk.W)
-    l_freenet.grid(row = 2, column = 0, sticky = tk.W)
+    l_onion.grid(row=0, column=0, sticky=tk.W)
+    l_i2p.grid(row=1, column=0, sticky=tk.W)
+    l_freenet.grid(row=2, column=0, sticky=tk.W)
+
 
 def run():
     root = tk.Toplevel()
