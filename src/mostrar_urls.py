b543f82dd337db0338d62f62d115f889efb6a0bd
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-07-12 07:19:15 -0300

    #!/usr/bin/env python3 e adequando o código ao PEP8.
    #!/usr/bin/env python3
    
    W293 blank line contains whitespace.
    26:1, 33:1, 36:1, 38:1, 41:1, 51:1, 58:1.
    
    W291 trailing whitespace.
    30:75, 31:66, 54:44.

--- src/mostrar_urls.py
+++ src/mostrar_urls.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import ttk
@@ -23,22 +24,22 @@ def mostrar_urls(self):
                self.wm_iconbitmap('ico\dw_ico.ico')
         except TclError:
                print('dw_ico.ico não foi encontrado na pasta ico.')
-                
+
     def tema_preto():
         self.config(bg = definir_cor.COR_PRETA)
         style = ttk.Style()
-        style.configure('urls.TLabel', background = definir_cor.COR_PRETA, 
-                        foreground = definir_cor.COR_VERDE_CLARO, 
+        style.configure('urls.TLabel', background = definir_cor.COR_PRETA,
+                        foreground = definir_cor.COR_VERDE_CLARO,
                         font = ('Times New Roman', '14'))
-    
+
     configurar_janela()
     configurar_icone()
-    
+
     tema_preto()
-    
+
     menubar = tk.Menu(self)
     self.config(menu = menubar)
-     
+
     ajuda = tk.Menu(menubar, tearoff = 0)
     menubar.add_cascade(label = 'Ajuda', menu = ajuda)
     ajuda.add_command(label = 'Redes', command = ajuda_redes.run)
@@ -48,14 +49,14 @@ def mostrar_urls(self):
     cursor.execute("""
     SELECT nome, url FROM urls;
     """)
-    
+
     x = 0
     for url in cursor.fetchall():
-        l_url = ttk.Label(self, text = url, 
+        l_url = ttk.Label(self, text = url,
                           style = 'urls.TLabel')
         l_url.grid(row = x, column = 0, sticky = tk.W)
         x += 1
-    
+
     conn.close()
 
 def run():
