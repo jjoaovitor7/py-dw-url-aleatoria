c69ca3029fbe79e3c4280fe140c80c0ef524b16f
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-07-16 11:40:32 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name='deep-web-url-aleatoria',
+    description='Programa que mostra uma URL aleatória da Deep Web.',
+    url='https://github.com/jjoaovitor7/deep-web-url-aleatoria',
+    packages=['src']
+)
