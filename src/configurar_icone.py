894794e44c016f84520f54354597798370b76bfe
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-08-05 09:25:42 -0300

    icon/icon.xbm e icon\icon.ico.
    self.wm_iconbitmap('@ico/dw_ico.xbm') -> self.wm_iconbitmap('@icon/icon.xbm')
    logging.warning('dw_ico.xbm não foi encontrado na pasta ico.') -> logging.warning('icon.xbm não foi encontrado na pasta ico.')
    self.wm_iconbitmap('ico\dw_ico.ico') -> self.wm_iconbitmap('icon\icon.ico')
    logging.warning('dw_ico.ico não foi encontrado na pasta ico.') -> logging.warning('icon.ico não foi encontrado na pasta ico.')

--- src/configurar_icone.py
+++ src/configurar_icone.py
@@ -5,21 +5,24 @@ from tkinter import ttk
 import platform
 import logging
 
+
 def configurar_icone(self):
     so = platform.system()
+
     try:
         if so == 'Linux':
-            self.wm_iconbitmap('@ico/dw_ico.xbm')
+            self.wm_iconbitmap('@icon/icon.xbm')
     except tk.TclError:
         logging.basicConfig(filename='warning.log',
                             format='[%(asctime)s] %(levelname)s: %(message)s',
                             level=logging.WARN)
-        logging.warning('dw_ico.xbm não foi encontrado na pasta ico.')
+        logging.warning('icon.xbm não foi encontrado na pasta ico.')
+
     try:
         if so == 'Windows':
-            self.wm_iconbitmap('ico\dw_ico.ico')
+            self.wm_iconbitmap('icon\icon.ico')
     except tk.TclError:
         logging.basicConfig(filename='warning.log',
                             format='[%(asctime)s] %(levelname)s: %(message)s',
                             level=logging.WARN)
-        logging.warning('dw_ico.ico não foi encontrado na pasta ico.')
+        logging.warning('icon.ico não foi encontrado na pasta ico.')
