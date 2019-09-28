026472a1d4ce6077e3bb70246b12aa0f90628de1
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-09-28 15:19:15 -0300

    Update mostrar_urls.py

--- src/mostrar_urls.py
+++ src/mostrar_urls.py
@@ -12,45 +12,51 @@ import ajuda_redes
 
 
 def mostrar_urls(self):
-    def configurar_janela():
+    def setTitle():
         self.title('URLs')
 
-    def tema_preto():
+
+    def blackTheme():
         self.config(bg=definir_cor.COR_PRETA)
         style = ttk.Style()
         style.configure('urls.TLabel', background=definir_cor.COR_PRETA,
                         foreground=definir_cor.COR_VERDE_CLARO,
                         font=('Times New Roman', '14'))
 
-    configurar_janela()
+
+    def setWidgets(url, x):
+        l_url = ttk.Label(self, text=url,
+                          style='urls.TLabel').grid(row=x,
+                                                    column=0,
+                                                    sticky=tk.W)
+
+
+    def readDB():
+        conn = sqlite3.connect('urls.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT nome, url FROM urls;')
+
+        x=0
+        for url in cursor.fetchall():
+            setWidgets(url, x)
+            x+=1
+
+        conn.close()
+
+
+    setTitle()
     configurar_icone.configurar_icone(self)
 
-    tema_preto()
+    blackTheme()
 
-    menubar = tk.Menu(self)
-    self.config(menu=menubar)
-
-    ajuda = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Ajuda', menu=ajuda)
-    ajuda.add_command(label='Redes', command=ajuda_redes.run)
-
-    conn = sqlite3.connect('urls.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-    SELECT nome, url FROM urls;
-    """)
-
-    x = 0
-    for url in cursor.fetchall():
-        l_url = ttk.Label(self, text=url,
-                          style='urls.TLabel')
-        l_url.grid(row=x, column=0, sticky=tk.W)
-        x += 1
-
-    conn.close()
+    readDB()
 
 
 def run():
     root = tk.Toplevel()
     mostrar_urls(root)
     root.mainloop()
+
+
+#if __name__ == '__main__':
+    #run()
