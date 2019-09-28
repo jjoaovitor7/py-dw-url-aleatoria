6ce95068e29c49b1dbe230bbb9fd7391a862aab5
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-09-28 20:41:30 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,12 +14,16 @@ import ajuda_redes
 
 
 def main(self):
-    def configurar_janela():
+    def setTitle():
         self.title('Deep Web URL Aleatória')
+
+
+    def setGeometry():
         self.geometry('500x75')
         self.grid_columnconfigure(0, weight=1)
 
-    def tema_preto():
+
+    def blackTheme():
         self.config(bg=definir_cor.COR_PRETA)
         style = ttk.Style()
         style.configure('main.TLabel', background=definir_cor.COR_PRETA,
@@ -29,7 +33,8 @@ def main(self):
                         foreground=definir_cor.COR_VERDE_CLARO,
                         width=5, relief=tk.RAISED)
 
-    def tema_branco():
+
+    def whiteTheme():
         self.config(bg=definir_cor.COR_BRANCA)
         style = ttk.Style()
         style.configure('main.TLabel', background=definir_cor.COR_BRANCA,
@@ -39,61 +44,68 @@ def main(self):
                         foreground=definir_cor.COR_PRETA,
                         width=5, relief=tk.RAISED)
 
-    configurar_janela()
+
+    def setMenus():
+        menubar = tk.Menu(self)
+        self.config(menu=menubar)
+
+        prefsMenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label='Preferências', menu=prefsMenu)
+
+        themesMenu = tk.Menu(menubar, tearoff=0)
+        prefsMenu.add_cascade(label='Temas', menu=themesMenu)
+        themesMenu.add_command(label='Tema Branco', command=whiteTheme)
+        themesMenu.add_command(label='Tema Preto', command=blackTheme)
+
+        helpMenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label='Ajuda', menu=helpMenu)
+        helpMenu.add_command(label='URLs', command=mostrar_urls.run)
+        helpMenu.add_command(label='Redes', command=ajuda_redes.run)
+
+        def aboutClicked():
+            messagebox.showinfo('Sobre', 'Programa criado por João Vítor S. F.' +
+                                '\nRepositório: ' +
+                                'github.com/jjoaovitor7/deep-web-url-aleatoria')
+
+        helpMenu.add_command(label='Sobre', command=aboutClicked)
+
+
+    def setWidgets():
+        titleLabel = ttk.Label(self, text='Deep Web URL Aleatória',
+                               style='main.TLabel').grid(row=0, column=0)
+
+        urlLabel = ttk.Label(self, text='URL', style='main.TLabel')
+        urlLabel.grid(row=1, column=0)
+
+        def goButtonClicked():
+            conn = sqlite3.connect('urls.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT url FROM urls ORDER BY RANDOM() LIMIT 1;')
+            getURLAux = cursor.fetchall()
+            getURL = str(getURLAux).strip('()[],\'\'')
+            #print(getURL)
+            urlLabel.config(text=getURL)
+            conn.close()
+
+            def URLClicked(event):
+                webbrowser.open_new(getURL)
+            urlLabel.bind('<Button-1>', URLClicked)
+
+        goButton = ttk.Button(self, text='Go!',
+                              style='main.TButton',
+                              command=goButtonClicked).grid(row=3,
+                                                            column=0)
+
+
     configurar_icone.configurar_icone(self)
 
-    tema_preto()
+    blackTheme()
 
-    menubar = tk.Menu(self)
-    self.config(menu=menubar)
+    setTitle()
+    setGeometry()
 
-    preferencias = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Preferências', menu=preferencias)
-
-    temas = tk.Menu(menubar, tearoff=0)
-    preferencias.add_cascade(label='Temas', menu=temas)
-
-    temas.add_command(label='Tema Branco', command=tema_branco)
-    temas.add_command(label='Tema Preto', command=tema_preto)
-
-    ajuda = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label='Ajuda', menu=ajuda)
-    ajuda.add_command(label='URLs', command=mostrar_urls.run)
-    ajuda.add_command(label='Redes', command=ajuda_redes.run)
-
-    def sobre_clicado():
-        messagebox.showinfo('Sobre', 'Programa criado por João Vítor S. F.' +
-                            '\nRepositório: ' +
-                            'github.com/jjoaovitor7/deep-web-url-aleatoria')
-
-    ajuda.add_command(label='Sobre', command=sobre_clicado)
-
-    l_titulo = ttk.Label(self, text='Deep Web URL Aleatória',
-                         style='main.TLabel')
-    l_titulo.grid(row=0, column=0)
-
-    l_url = ttk.Label(self, text='URL', style='main.TLabel')
-    l_url.grid(row=1, column=0)
-
-    def b_go_clicado():
-        conn = sqlite3.connect('urls.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-        SELECT url FROM urls  ORDER BY RANDOM() LIMIT 1;
-        """)
-        get_url = cursor.fetchall()
-        get_url_ = str(get_url).strip('()[],\'\'')
-        l_url.config(text=get_url)
-        conn.close()
-
-        def clicar_url(event):
-            webbrowser.open_new(get_url_)
-        l_url.bind('<Button-1>', clicar_url)
-
-    b_go = ttk.Button(self, text='Go!',
-                      style='main.TButton',
-                      command=b_go_clicado)
-    b_go.grid(row=3, column=0)
+    setMenus()
+    setWidgets()
 
 
 def run():
